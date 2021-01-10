9fc2c0d8e308a60de325c4332f61b415f622a243
Diego Cortes <diegocortes4@gmail.com>
2021-01-10 18:41:55 -0500

    update exercise schema

--- models/exercises.js
+++ models/exercises.js
@@ -3,19 +3,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise"
+            },
+            duration: { type: Number },
+            weight: { type: Number },
+            reps: { type: Number },
+            sets: { type: Number },
+            distance: { type: Number }
+        }
+    ]
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
